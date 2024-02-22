051bbdb63575c8af807951e58acdd39194e04426
AlGaJin <agalanj16@iesalbarregas.es>
2024-02-22 18:36:23 +0100

    Creación primera vista
    Primera disposición de la vista de la toma de la comanda.

---
+++ src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class PrimaryController {
+
+    public void initialize(){
+
+    }
+}
