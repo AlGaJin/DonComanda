8259700e28faa31253248cada351eb3aa196703e
AlGaJin <agalanj16@iesalbarregas.es>
2024-02-28 20:54:10 +0100

    Primera muestra de la vista
    No hay nada funcional aún, se está trabajando la parte visual

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,15 +1,16 @@
 package org.example;
 
-import java.io.IOException;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
     public void initialize(){
+
+    }
+
+    @FXML
+    public void calculadora(Event e){
 
     }
 }
